39f64905699323da434db3755f7d46cf108abc4f
Anurag Singh Rajput <anuragrajput8477@gmail.com>
2024-09-01 16:36:31 +0530

    Initial commit for backend

---
+++ app.js
@@ -0,0 +1,57 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { dbConnection } from "./database/dbConnection.js";
+import messageRouter from "./router/messageRouter.js";
+import { errorMiddleware } from './middlewares/errorMiddleware.js';
+import userRouter from './router/userRouter.js';
+import appointmentRouter from "./router/appointmentRouter.js";
+
+// Load environment variables
+config({ path: ".env" });
+
+const app = express();
+
+// Middleware to log request and response headers for debugging
+app.use((req, res, next) => {
+  console.log("Request Headers:", req.headers);
+  res.on("finish", () => {
+    console.log("Response Headers:", res.getHeaders());
+  });
+  next();
+});
+
+// CORS configuration
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Corrected option name
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+// Route handlers
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+// Database connection
+dbConnection();
+
+// Error middleware
+app.use(errorMiddleware);
+
+export default app;
